c8997874d3fa2f0836ffc4dfb71b910d521fc34c
Plus10Victory <plustenvictory@gmail.com>
2025-07-20 22:46:02 -0700

    cleanInput with first unit test added

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	text := "Hello, World!"
+	fmt.Println("input: " + text)
+	for _, word := range cleanInput(text) {
+		fmt.Println(word)
+	}
+}
+
+func cleanInput(text string) []string {
+	return strings.Fields(strings.ToLower(text))
 }
