b8857d37cb307fea6ce621e35666795d0ab0f2a4
Plus10Victory <plustenvictory@gmail.com>
2025-07-21 23:19:23 -0700

    created initial logic for the Repl loop

--- main.go
+++ main.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	text := "Hello, World!"
-	fmt.Println("input: " + text)
-	for _, word := range cleanInput(text) {
-		fmt.Println(word)
-	}
+	startRepl()
 }
